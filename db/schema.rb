5aa2c4eb083f43804e77da8a44cefcf16becd5a7
Ben Purinton <ben.purinton@gmail.com>
2023-07-25 09:44:22 -0800

    try to fix db again

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2019_09_25_193522) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "actors", force: :cascade do |t|
     t.string "name"
     t.date "dob"
